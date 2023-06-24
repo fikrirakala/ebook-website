81a3bd3dbf1489e5507de2126b556f25d72d6642
Fikri Rakala <fikrirakala@gmail.com>
2023-06-24 18:45:52 +0800

    add navbar scroll effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// Add custom JavaScript here
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", function () {
+    if (this.scrollY > 100) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-sticky");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
